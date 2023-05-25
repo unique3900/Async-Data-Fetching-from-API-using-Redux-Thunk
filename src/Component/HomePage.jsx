6b6487923b00debadf10ccef24f53dced5c02b08
uniqueneu3900 <62101934+unique3900@users.noreply.github.com>
2023-05-25 20:22:43 +0545

    fetched data displayed

--- src/Component/HomePage.jsx
+++ src/Component/HomePage.jsx
@@ -2,10 +2,21 @@ import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchData } from '../Redux/Slices/slice';
 
+import '../App.css'
 const HomePage = () => {
     const dispatcher = useDispatch();
     const selector = useSelector((state) => state);
-    console.log(selector);
+
+    if (selector.loadingStatus) {
+        return (
+            <h1>Loading Data.........</h1>
+        )
+    }
+    else if (selector.errorStatus) {
+        return (
+            <h1>Error Fetching Data</h1>
+        )
+    }
   return (
       <div>
           {
@@ -13,10 +24,31 @@ const HomePage = () => {
                 <button onClick={(e) => {
                     dispatcher(fetchData())
             }}>Fetch</button>
-            ) : " hey"
+              ) : ("")
           }
-
+ <div className="container" >
           
+          {
+              
+             selector.datas && selector.datas.slice(0,30).map((item, idex) => {
+                  return (
+                   
+                    <div className="card" key={idex}>
+                        <div className="imageBox">
+                            <img src={item.thumbnailUrl
+} alt="" />
+                        </div>
+                        <div className="title">
+                              <h3>{item.title}</h3>
+                        </div>
+                    </div>
+          
+               
+                  )
+             })
+                  
+              }
+               </div>  
 
     </div>
   )
